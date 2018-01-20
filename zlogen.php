66aa4e2acd657625e9fb1204a4072350a8e27967
naweak <33986066+naweak@users.noreply.github.com>
2018-01-20 14:39:42 +0300

    Create zlogen.php

---
+++ zlogen.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Генератор зла</title>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.0/jquery.min.js"></script>
+</head>
+<body>
+	<textarea name="zlo" id="zlo" style="width:100%; height:500px;"></textarea>
+	Мощность: <input type="text" name="power" id="power" value="20" /> Со ссылками 
+	<select name="" id="with_links">
+		<option value="0">Нет</option>
+		<option value="1">Да</option>
+	</select>
+	<span class="gsom" style="background:gray;cursor:pointer;">ПЫЩЬ!</span>
+	<script>
+		$('span.gsom').click(function() {
+			$.ajax({
+				type:"GET",
+				url:"ajax/zlogen.php",
+				data:{
+					power:$('input#power').val(),
+					with_links:$('#with_links').val()
+				},
+				success:function(data) {
+					$('textarea#zlo').val(data);
+				}
+			});
+		});
+	</script>
+</body>
+</html>
